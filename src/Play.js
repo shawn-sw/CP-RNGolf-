e6f5bef4636f4b92192373dfd47d40de1b950bcf
“Shaoan <swang387@ucsc.edu>
2024-02-02 22:16:33 -0800

    first commit

---
+++ src/Play.js
@@ -0,0 +1,135 @@
+class Play extends Phaser.Scene {
+    constructor() {
+        super('playScene')
+    }
+
+    init() {
+        // useful variables
+        this.SHOT_VELOCITY_X = 200
+        this.SHOT_VELOCITY_Y_MIN = 700
+        this.SHOT_VELOCITY_Y_MAX = 1100
+    }
+
+    preload() {
+        this.load.path = './assets/img/'
+        this.load.image('grass', 'grass.jpg')
+        this.load.image('cup', 'cup.jpg')
+        this.load.image('ball', 'ball.png')
+        this.load.image('wall', 'wall.png')
+        this.load.image('oneway', 'one_way_wall.png')
+    }
+
+    create() {
+
+        // add background grass
+        this.grass = this.add.image(0, 0, 'grass').setOrigin(0)
+        this.grass.setDepth(0)
+
+
+        // add counter, score, and successful shot percentage
+        this.shotCount = 0
+        this.totalShotCount = 0
+        this.count = 1
+        this.successfulShotPercentage = 0
+
+        let scoreConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+            top: 5,
+            bottom: 5,
+            },
+            fixedWidth: 0
+        }
+
+        this.countLeft = this.add.text(20, 20, 'count:' + this.count,scoreConfig).setOrigin(0)
+        this.scoreLeft = this.add.text(20, 60, 'shot:' + this.shotCount,scoreConfig).setOrigin(0)
+        this.successfulShotPercentageLeft = this.add.text(width-20, 60, 'successful:' + this.shotCount + ' %',scoreConfig).setOrigin(1)
+
+
+        // add cup
+        this.cup = this.physics.add.sprite(width / 2, height / 10,'cup')
+        this.cup.body.setCircle(this.cup.width /4)
+        this.cup.body.setOffset(this.cup.width / 4)
+        this.cup.body.setImmovable(true)
+        
+        // add ball
+        this.ball = this.physics.add.sprite(width / 2, height - height / 10, 'ball')
+        this.ball.body.setCircle(this.ball.width / 2)
+        this.ball.body.setCollideWorldBounds(true)
+        this.ball.body.setBounce(0.5)
+        this.ball.body.setDamping(true).setDrag(0.5)
+
+        // add walls
+        let wallA = this.physics.add.sprite(0, height / 4, 'wall')
+        wallA.setX(Phaser.Math.Between(0 + wallA.width/2, width - wallA.width/2))
+        wallA.body.setCollideWorldBounds(true)
+        wallA.body.setImmovable(true)
+        wallA.setVelocityX(130)
+        wallA.setBounce(1)
+
+        let wallB = this.physics.add.sprite(0, height / 2, 'wall')
+        wallB.setX(Phaser.Math.Between(0 + wallB.width/2, width - wallB.width/2))
+        wallB.body.setCollideWorldBounds(true)
+        wallB.body.setImmovable(true)
+        wallB.setVelocityX(100)
+        wallB.setBounce(1)
+
+        this.walls = this.add.group([wallA, wallB])
+
+        // add one-way
+        this.oneWay = this.physics.add.sprite(width/2, height/4*3, 'oneway')
+        this.oneWay.setX(Phaser.Math.Between(0 + this.oneWay.width/2, width - this.oneWay.width/2))
+        this.oneWay.body.setImmovable(true)
+        this.oneWay.body.checkCollision.down = false
+
+        // add pointer input
+        this.input.on('pointerdown',(pointer) => {
+            this.shotCount += 1
+            this.totalShotCount += 1
+            this.scoreLeft.text = 'shot:' + this.shotCount
+            let shotDirectionY = pointer.y <= this.ball.y ? 1:-1
+            let shotDirectionX = pointer.x <= this.ball.x ? 1:-1
+            this.ball.body.setVelocityX(Phaser.Math.Between(this.SHOT_VELOCITY_X, this.SHOT_VELOCITY_X)* shotDirectionX)
+            this.ball.body.setVelocityY(Phaser.Math.Between(this.SHOT_VELOCITY_Y_MIN, this.SHOT_VELOCITY_Y_MAX) * shotDirectionY)
+        })
+
+        // cup/ball collision   
+        // reset the ball and walls when successful shot
+        this.physics.add.collider(this.ball, this.cup, () =>{
+            this.successfulShotPercentageLeft.text = 'successful:' + ((this.count / this.totalShotCount )* 100).toFixed(2) + ' %'
+            this.shotCount = 0
+            this.count += 1
+            this.scoreLeft.text = 'shot:' + this.shotCount
+            this.countLeft.text = 'count:' + this.count
+            this.ball.setX(width / 2)
+            this.ball.setY(height - height / 10)
+            wallA.setX(Phaser.Math.Between(0 + wallA.width/2, width - wallA.width/2))
+            wallB.setX(Phaser.Math.Between(0 + wallB.width/2, width - wallB.width/2))
+            this.oneWay.setX(Phaser.Math.Between(0 + this.oneWay.width/2, width - this.oneWay.width/2))
+            this.ball.setVelocityX(0)
+            this.ball.setVelocityY(0)
+        })
+
+        // ball/wall collision
+        this.physics.add.collider(this.ball, this.walls)
+
+        // ball/one-way collision
+        this.physics.add.collider(this.ball, this.oneWay)
+    }
+
+    update() {
+    }
+
+}
+/*
+CODE CHALLENGE
+Try to implement at least 3/4 of the following features during the remainder of class (hint: each takes roughly 15 or fewer lines of code to implement):
+[x] Add ball reset logic on successful shot
+[x] Improve shot logic by making pointer’s relative x-position shoot the ball in correct x-direction
+[x] Make one obstacle move left/right and bounce against screen edges
+[x] Create and display shot counter, score, and successful shot percentage
+*/
